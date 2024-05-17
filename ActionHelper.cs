b1f5030dd2e53eb4ee9499a8b73558eab727051d
niltor <zpty@outlook.com>
2024-05-17 18:28:16 +0800

    test ocr

---
+++ ActionHelper.cs
@@ -0,0 +1,230 @@
+﻿using Tesseract;
+
+namespace GameAssistant;
+public class ActionHelper
+{
+    internal HWND TargetWindow { get; set; }
+    /// <summary>
+    /// 窗口宽
+    /// </summary>
+    public int Width { get; private set; }
+    /// <summary>
+    /// 窗口高
+    /// </summary>
+    public int Height { get; private set; }
+
+    public int WidthPix { get; private set; } = 900;
+    public int HeightPix { get; private set; } = 1600;
+
+    public ActionHelper(string windowName)
+    {
+        TargetWindow = PInvoke.FindWindow(null, windowName);
+        if (TargetWindow == IntPtr.Zero)
+        {
+            throw new ArgumentException($"未找到窗口{windowName}");
+        }
+        // 获取窗口大小
+        PInvoke.GetClientRect(TargetWindow, out RECT rect);
+
+        Width = rect.right - rect.left;
+        Height = rect.bottom - rect.top;
+
+        Console.WriteLine($"窗口大小: 宽度={rect.right - rect.left}, 高度={rect.bottom - rect.top}");
+    }
+
+    /// <summary>
+    /// 设置分辨率
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    public void SetStandardSize(int width, int height)
+    {
+        WidthPix = width;
+        HeightPix = height;
+    }
+
+    public static Process[] EnumWindows()
+    {
+        return Process.GetProcesses()
+            .Where(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle))
+            .ToArray();
+    }
+
+    public void EnableWindow()
+    {
+        PInvoke.EnableWindow(TargetWindow, true);
+        PInvoke.SetFocus(TargetWindow);
+        PInvoke.SetCapture(TargetWindow);
+        PInvoke.SetForegroundWindow(TargetWindow);
+    }
+
+    /// <summary>
+    /// 按键
+    /// </summary>
+    /// <param name="vIRTUAL_KEY"></param>
+    /// <param name="pressTime">按下到松开的时间</param>
+    internal void PressKey(VIRTUAL_KEY vIRTUAL_KEY, int pressTime = 5)
+    {
+        Span<INPUT> inputs = [
+            CreateKeyBoardInput(vIRTUAL_KEY),
+        ];
+        var res = PInvoke.SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+
+        Task.Delay(pressTime).Wait();
+
+        inputs = [
+            CreateKeyBoardInput(vIRTUAL_KEY,KEYBD_EVENT_FLAGS.KEYEVENTF_KEYUP),
+        ];
+        res = PInvoke.SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+
+        if (res == 0)
+        {
+            var message = Marshal.GetLastPInvokeErrorMessage();
+            Console.WriteLine("PressKey error: " + message);
+        }
+    }
+
+    internal void Click(int x, int y)
+    {
+        ClientToScreen(ref x, ref y);
+        PInvoke.SetCursorPos(x, y);
+        Span<INPUT> inputs = [
+            CreateMouseInput(MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTDOWN, x, y),
+            CreateMouseInput(MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTUP, x, y),
+        ];
+        var res = PInvoke.SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+        if (res == 0)
+        {
+            var message = Marshal.GetLastPInvokeErrorMessage();
+            Console.WriteLine("Click error: " + message);
+        }
+    }
+
+    /// <summary>
+    /// 鼠标输出
+    /// </summary>
+    /// <param name="eVENT_FLAGS"></param>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    private INPUT CreateMouseInput(MOUSE_EVENT_FLAGS eVENT_FLAGS, int x, int y)
+    {
+        var mouseInput = new MOUSEINPUT
+        {
+            dx = x,
+            dy = y,
+            dwFlags = eVENT_FLAGS,
+            mouseData = 0,
+        };
+
+        return new INPUT
+        {
+            type = INPUT_TYPE.INPUT_MOUSE,
+            Anonymous = new INPUT._Anonymous_e__Union
+            {
+                mi = mouseInput
+            }
+        };
+    }
+    /// <summary>
+    /// 键盘输入
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="eVENT_FLAGS"></param>
+    /// <returns></returns>
+    private static INPUT CreateKeyBoardInput(VIRTUAL_KEY key, KEYBD_EVENT_FLAGS? eVENT_FLAGS = null)
+    {
+        var keyBoardInput = new KEYBDINPUT
+        {
+            wVk = key,
+        };
+        if (eVENT_FLAGS != null)
+        {
+            keyBoardInput.dwFlags = eVENT_FLAGS.Value;
+        }
+
+        return new INPUT
+        {
+            type = INPUT_TYPE.INPUT_KEYBOARD,
+            Anonymous = new INPUT._Anonymous_e__Union
+            {
+                ki = keyBoardInput
+            }
+        };
+    }
+
+    public Bitmap CaptureScreen(int x, int y, int width, int height)
+    {
+        Bitmap bmp = new Bitmap(width, height);
+        using (Graphics g = Graphics.FromImage(bmp))
+        {
+            HDC hdcDest = (HDC)g.GetHdc();
+            HDC hdcSrc = PInvoke.GetDC(TargetWindow);
+
+            PInvoke.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, x, y, ROP_CODE.SRCCOPY);
+            PInvoke.ReleaseDC(TargetWindow, hdcSrc);
+            g.ReleaseHdc(hdcDest);
+        }
+        return bmp;
+    }
+
+    public Color GetPixelColor(int x, int y)
+    {
+        HDC hdc = PInvoke.GetDC(TargetWindow);
+        uint pixel = PInvoke.GetPixel(hdc, x, y);
+        PInvoke.ReleaseDC(TargetWindow, hdc);
+
+        int r = (int)(pixel & 0x000000FF);
+        int g = (int)(pixel & 0x0000FF00) >> 8;
+        int b = (int)(pixel & 0x00FF0000) >> 16;
+        return Color.FromArgb(r, g, b);
+    }
+
+    public string GetTextFromOCR(byte[] bytes)
+    {
+        using (var engine = new TesseractEngine("./", "eng", EngineMode.Default))
+        {
+            using (var img = Pix.LoadFromMemory(bytes))
+            {
+                using (var page = engine.Process(img))
+                {
+                    string text = page.GetText();
+                    Console.WriteLine($"识别的文本：{text}");
+                    return text;
+                }
+            }
+        }
+    }
+
+    public string GetTextFromFile(string path)
+    {
+        using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+        {
+            using (var img = Pix.LoadFromFile(path))
+            {
+                using (var page = engine.Process(img))
+                {
+                    string text = page.GetText();
+                    Console.WriteLine($"识别的文本：{text}");
+                    return text;
+                }
+            }
+        }
+    }
+
+
+
+    /// <summary>
+    /// 客户端转屏幕坐标
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    private void ClientToScreen(ref int x, ref int y)
+    {
+        Point point = new() { X = x, Y = y };
+
+        PInvoke.ClientToScreen(TargetWindow, ref point);
+        x = point.X;
+        y = point.Y;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GameAssistant;
 
+var allProcesses = ActionHelper.EnumWindows();
+foreach (Process process in allProcesses)
+{
+    // 输出 process 信息,如果有多个窗口，全部输出 
+    Console.WriteLine($"[{process.Id}]:{process.ProcessName},({process.MainWindowHandle}), => {process.MainWindowTitle}");
+}
 
-Helper.EnumWindows();
-var windowHandler = Helper.GetWindow("Untitled - Paint");
-Helper.SetCursorAndClick(windowHandler, 500, 500);
+
+
+await Test();
+//var helper = new ActionHelper("MuMu模拟器12");
+//var helper = new ActionHelper("dict.component.html - ClientApp - Visual Studio Code");
+
+//helper.EnableWindow();
+//await Task.Delay(200);
+
+//helper.PressKey(VIRTUAL_KEY.VK_A);
+//helper.PressKey(VIRTUAL_KEY.VK_A);
+
+//helper.Click(500, 500);
+
+
+
+static async Task Test()
+{
+    var helper = new ActionHelper("MuMu模拟器12");
+    helper.EnableWindow();
+    await Task.Delay(200);
+
+    var ocrPath = @"./test.png";
+    helper.GetTextFromFile(ocrPath);
+
+}
+
+
+
